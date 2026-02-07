5d5278fcd947b780b35eb08ebab46e072a38941a
Aashish Khadka <aashishkhadka720@gmail.com>
2026-02-08 03:33:45 +0545

    updated the cors for vercel deployed frontend

--- Frontend/src/servies/axiosInstance.js
+++ Frontend/src/servies/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/api/v1'
+const baseURL = 'https://internshipbackend.vercel.app/api/v1'
 const axiosInstance = axios.create({
     baseURL: baseURL,
     headers:{
